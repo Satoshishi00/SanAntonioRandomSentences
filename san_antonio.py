99d677b3003be317ace6b01dba5df5afc64f0ef1
Satoshishi00 <webmaster-2.0@hotmail.com>
2020-02-24 15:37:09 +0100

    Ajout aléatoire de d'autres personnages depuis un JSON contenu dans un autre fichier

--- san_antonio.py
+++ san_antonio.py
@@ -1,4 +1,5 @@
 import random
+import json
 
 quotes = [
     "Ecoutez-moi, Monsieur Shakespeare, nous avons beau être ou ne pas être, nous sommes !",
@@ -26,9 +27,25 @@ def get_random_item_in(my_list):
     item = my_list[rand_nb] # get a quote from a list
     return item # return the item
 
+def read_value_from_json():
+    new_characters = []
+
+    with open("characters.json", "r") as file:
+        characters_dico = json.load(file)
+
+    for item in characters_dico:
+        new_characters.append(item["character"])
+
+    return new_characters
+
+def get_random_character():
+    all_characters = characters + read_value_from_json()
+    rand_character = get_random_item_in(all_characters)
+    return rand_character
+
 # Programm
 user_answer = input('Tapez entrée pour connaître une autre citation ou B pour quitter le programme.')
 
 while user_answer != "B":
-    print(message(get_random_item_in(characters), get_random_item_in(quotes)))
+    print(message(get_random_character(), get_random_item_in(quotes)))
     user_answer = input('Tapez entrée pour connaître une autre citation ou B pour quitter le programme.')
