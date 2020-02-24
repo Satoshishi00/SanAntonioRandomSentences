8016124d4a69f50568d4d16dbbe17eae3b08cd81
Satoshishi00 <webmaster-2.0@hotmail.com>
2020-02-24 15:49:43 +0100

    Ajout de quotes dans un fichier à part sous forme de json

--- san_antonio.py
+++ san_antonio.py
@@ -27,25 +27,30 @@ def get_random_item_in(my_list):
     item = my_list[rand_nb] # get a quote from a list
     return item # return the item
 
-def read_value_from_json():
+def read_value_from_json(fileName, key):
     new_characters = []
 
-    with open("characters.json", "r") as file:
+    with open(fileName, "r") as file:
         characters_dico = json.load(file)
 
     for item in characters_dico:
-        new_characters.append(item["character"])
+        new_characters.append(item[key])
 
     return new_characters
 
 def get_random_character():
-    all_characters = characters + read_value_from_json()
+    all_characters = characters + read_value_from_json("characters.json", "character")
     rand_character = get_random_item_in(all_characters)
     return rand_character
+
+def get_random_quote():
+    all_quotes = quotes + read_value_from_json("quotes.json", "quote")
+    rand_quote = get_random_item_in(all_quotes)
+    return rand_quote
 
 # Programm
 user_answer = input('Tapez entrée pour connaître une autre citation ou B pour quitter le programme.')
 
 while user_answer != "B":
-    print(message(get_random_character(), get_random_item_in(quotes)))
+    print(message(get_random_character(), get_random_quote()))
     user_answer = input('Tapez entrée pour connaître une autre citation ou B pour quitter le programme.')
